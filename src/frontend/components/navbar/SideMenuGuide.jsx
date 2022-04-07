22efea5cb8d02d8ef6540b002189d58f2fa13746
ramalingaa <87629304+ramalingaa@users.noreply.github.com>
2022-04-07 14:22:57 +0530

    Styles

--- src/frontend/components/navbar/SideMenuGuide.jsx
+++ src/frontend/components/navbar/SideMenuGuide.jsx
@@ -7,11 +7,11 @@ const SideMenuGuide = ({singlePage = false}) => {
   return (
     <div className ="side-navbar-wrapper">
        <div className = {`side-navbar-content ${singlePage && "singlepage-sidebar"} ${theme}`}>
-            <NavLink to ="/videos" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className="fas fa-rss sidebar-icon" title = {singlePage ? "Latest Videos" :""}></i>{ !singlePage && "Latest Videos"}</NavLink>
-            <NavLink to ="/WatchLater" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className="fas fa-alarm-clock sidebar-icon" title = {singlePage ? "Watch Later" :""}></i>{ !singlePage && "Watch Later"}</NavLink> 
-            <NavLink to = "/LikedVideo" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className="fas fa-thumbs-up sidebar-icon" title = {singlePage ? "Liked Videos" :""}></i>{ !singlePage && "Liked Videos" }</NavLink>
-            <NavLink to = "/History" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className="fas fa-history sidebar-icon" title = {singlePage ? "History" :""}></i>{!singlePage && "History"}</NavLink>
-            <NavLink to = "/Playlist" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className="far fa-hdd sidebar-icon" title = {singlePage ? "Playlist" :""}></i>{!singlePage && "Playlist"}</NavLink>
+            <NavLink to ="/videos" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className={`fas fa-rss ${singlePage ?"sidebar-singlepage-icon" :"sidebar-icon"}`} title = {singlePage ? "Latest Videos" :""}></i>{ !singlePage && "Latest Videos"}</NavLink>
+            <NavLink to ="/WatchLater" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className={`fas fa-alarm-clock ${singlePage ?"sidebar-singlepage-icon" :"sidebar-icon"}`} title = {singlePage ? "Watch Later" :""}></i>{ !singlePage && "Watch Later"}</NavLink> 
+            <NavLink to = "/LikedVideo" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className={`fas fa-thumbs-up ${singlePage ?"sidebar-singlepage-icon" :"sidebar-icon"}`} title = {singlePage ? "Liked Videos" :""}></i>{ !singlePage && "Liked Videos" }</NavLink>
+            <NavLink to = "/History" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className={`fas fa-history ${singlePage ?"sidebar-singlepage-icon" :"sidebar-icon"}`} title = {singlePage ? "History" :""}></i>{!singlePage && "History"}</NavLink>
+            <NavLink to = "/Playlist" className = {!singlePage && (({isActive}) => isActive ? "link-active":"link-inactive")}><i className={`far fa-hdd ${singlePage ?"sidebar-singlepage-icon" :"sidebar-icon"}`} title = {singlePage ? "Playlist" :""}></i>{!singlePage && "Playlist"}</NavLink>
         </div>
     </div>
   )
